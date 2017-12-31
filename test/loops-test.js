db1d9c47155071e21267b94d6ae72ee605757ec1
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 03:39:30 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -36,7 +36,8 @@ describe('loops', () => {
       const testArray = strangeArray.slice(array.length)
 
       for (let i = 0, l = testArray.length; i < l; i++) {
-        let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`;
+        let s;
+        if (i===1){s="I am 1 strange loop."}else{s=`I am ${i} strange loops.`}
         expect(testArray[i]).to.equal(s)
       }
     })
