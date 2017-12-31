5cec64c944fb8ba45c0d2002c76ec86c123a2af0
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 02:56:17 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array){
+  let result=[];
+  for (i=0;i<(25+array.length);i++){
+    if(i===1){result.push(`I am 1 strange loop.`)}
+    else{result.push(`I am ${i} strange loops.`)}}
+  console.log(result);console.log(array);return result;}
+
+function whileLoop(number){
+  while(i>0){i
+    console.log(`Countdown counter at: ${number}`);number--;}return'done';}
+    
+    let testArray=[1,2,3,4,5,6,7];
+  
+
+//whileLoop(5);
+ // forLoop(testArray);
+  //console.log(testArray);
